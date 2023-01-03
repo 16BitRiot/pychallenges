1cfb0cb3aa9136c989283c4f3bed7e510c6bc461
Philip Fryer <109255121+16BitRiot@users.noreply.github.com>
2023-01-03 18:47:30 -0500

    commit

--- 14_compact/compact.py
+++ 14_compact/compact.py
@@ -1,6 +1,17 @@
 def compact(lst):
+    updated_lst = []
+    for item in lst:
+        if bool(item) == True:
+            updated_lst.append(item)
+
+    print(updated_lst)
+    return updated_lst
     """Return a copy of lst with non-true elements removed.
 
         >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
         [1, 2, 'All done']
     """
+
+compact([0, 1, 2, '', [], False, (), None, 'All done'])
+
+# tested, works
